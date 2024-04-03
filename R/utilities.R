1df29d83daf8d5290ec5c03d86a70d704842dcb9
xiangpin <xshuangbin@163.com>
2024-04-03 20:21:57 +0800

    introduce donut_radius and bg_circle_radius parameters

--- R/utilities.R
+++ R/utilities.R
@@ -16,3 +16,22 @@ is_fixed_radius <- function(rvar) {
     }
     return(TRUE)
 }
+
+.check_donut_radius <- function(x){
+    if (x > 1){
+        cli::cli_warn("The `donut.radius` should be range 0 and 1, it was set to 0.5 automatically.")
+        x <- 0.5
+    }
+    return(x)
+}
+
+.extract_mapping_df <- function(data,
+                                mapping, 
+                                extract_aes = c('x0', 'y0'),
+                                col_var = NULL
+                                ){
+    extract.var <- lapply(extract_aes, function(x)get_aes_var(mapping, x)) |> unlist()
+    extract.var <- union(col_var, extract.var)
+    df <- data[, colnames(data) %in% extract.var, drop=FALSE] |> dplyr::distinct()
+    return(df)
+}
